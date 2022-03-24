a7f71eec7fb1eee70f1ba5503ca28dcdf7fc7c88
unverzed <vitoriarortega@gmail.com>
2022-03-23 21:18:54 -0300

    feat: inserting header layout

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
+import Header from "./components/header/header"
+
 export default function App() {
     return (
         <>
-        <h1>Teste</h1>
+        <Header />
         </>
     )
 }
---
+++ src/components/header/header.js
@@ -0,0 +1,9 @@
+import "./header.css";
+
+export default function Header() {
+    return (
+        <header className="header">
+         <h1 className="header-title">CINEFLIX</h1>
+        </header>
+    )
+}
